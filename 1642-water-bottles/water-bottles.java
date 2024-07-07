2d734d3caa0f7e995b2ddfceecd0c500969cb4e3
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-07-07 19:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (54.33%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        if(numBottles < numExchange) return numBottles;
+        return numBottles + (numBottles-1)/(numExchange-1);
+    }
+}
