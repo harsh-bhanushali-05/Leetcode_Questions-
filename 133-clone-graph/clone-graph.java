bf6e32a2b73685b2428e64273fa4e022255ab0e4
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-07-10 10:16:10 +0530

    Time: 26 ms (66.79%) | Memory: 42.5 MB (79.39%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public void f(Node node ,HashSet<Node> vis , HashMap<Node , Node> map ){
+        if(vis.contains(node))return;
+        vis.add(node);
+        Node naya = new Node(node.val);
+        map.put(node , naya);
+        for(Node nbr : node.neighbors){
+            f(nbr , vis , map);
+        }
+    }
+    public void link(Node node ,HashMap<Node , Node> map ,HashSet<Node> vis){
+        if(vis.contains(node))return;
+        vis.add(node);
+        for(Node nbr : node.neighbors){
+            map.get(node).neighbors.add(map.get(nbr));
+        }
+        for(Node nbr : node.neighbors){
+            link(nbr , map , vis);
+        }
+    }
+    public Node cloneGraph(Node node) {
+        if(node == null)return node;
+        HashMap<Node , Node>  map = new HashMap<>(); // old , new node 
+        HashSet<Node> vis = new HashSet<>();
+        f(node , vis , map); // assigns old node to new node 
+        link(node , map ,new HashSet<>());
+        return map.get(node);
+    }
+}
