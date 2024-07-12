357aee7db649a042ee7de815f909c708c7237e05
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-07-12 10:47:12 +0530

    Time: 7 ms (49.12%) | Memory: 43.9 MB (86.10%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,45 @@
+class Solution {
+    class pair{
+        int node; 
+        int wt;
+        pair(int node , int wt ){
+            this.node = node;
+            this.wt =wt;
+        }
+    }
+    class tuple{
+        int node;
+        int wt;
+        int stops;
+        tuple(int node , int wt , int stops ){
+            this.node = node;
+            this.wt=wt;
+            this.stops= stops ;
+        }
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        HashMap<Integer,List<pair>> map = new HashMap<>();
+        for(int i = 0 ; i < n ; i++){
+            map.put(i , new ArrayList<>());
+        }
+        for(int i = 0 ; i < flights.length ; i++){
+            map.get(flights[i][0]).add(new pair(flights[i][1] , flights[i][2]));
+        }
+        PriorityQueue<tuple> pq = new PriorityQueue<>((a,b)->a.stops-b.stops);
+        int dist[] = new int[n];
+        Arrays.fill(dist , (int) 1e9);
+        dist[src] = 0;
+        pq.offer(new tuple(src , 0 , 0 ));
+        while(pq.isEmpty()==false){
+            tuple temp = pq.poll();
+            if(temp.stops>k)continue;
+            for(pair p : map.get(temp.node)){
+               if(dist[p.node] > temp.wt + p.wt){
+                    dist[p.node] = temp.wt + p.wt;
+                    pq.offer(new tuple(p.node , dist[p.node] , temp.stops+1));
+               }
+            }
+        }
+        return dist[dst]>(int)1e8?-1:dist[dst];
+    }
+}
