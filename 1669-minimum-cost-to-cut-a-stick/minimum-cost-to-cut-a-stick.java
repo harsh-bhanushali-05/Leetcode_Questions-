fa830924e07f482723a43d348efa39a349042d71
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-07-13 19:49:34 +0530

    Time: 11 ms (89.53%) | Memory: 42.6 MB (26.04%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,24 +1,28 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        int m = cuts.length;
-        Arrays.sort(cuts);
-        int[] newCuts = new int[m + 2];
-        System.arraycopy(cuts, 0, newCuts, 1, m);
-        newCuts[0] = 0;
-        newCuts[m + 1] = n;
-        
-        int[][] dp = new int[m + 2][m + 2];
-        
-        for (int length = 2; length <= m + 1; length++) {
-            for (int i = 0; i + length <= m + 1; i++) {
-                int j = i + length;
-                dp[i][j] = Integer.MAX_VALUE;
-                for (int k = i + 1; k < j; k++) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + newCuts[j] - newCuts[i]);
-                }
-            }
+        int arr[]=new int[cuts.length+2];
+        for(int i=0;i<cuts.length;i++)
+            arr[i+1]=cuts[i];
+        arr[0]=0;
+        arr[arr.length-1]=n;
+        Arrays.sort(arr);
+        int dp[][]=new int[cuts.length+2][cuts.length+2];
+        for(int i=0;i<dp.length;i++)
+            Arrays.fill(dp[i],0);
+
+        for(int i=cuts.length;i>0;i--){
+            for(int j=1;j<=cuts.length;j++){
+                if(i>j)continue;
+
+                int min=Integer.MAX_VALUE;
+                    for(int k=i;k<=j;k++){
+                        int ans=arr[j+1]-arr[i-1]+dp[i][k-1]+dp[k+1][j];
+                        min=Math.min(min,ans);
+                    }
+                dp[i][j]=min;
+            }   
         }
         
-        return dp[0][m + 1];
+        return dp[1][cuts.length];
     }
-}
+}// this is the tabulation solution 
