a911c697b81671651b310d8d14dc14decd8b81e7
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-07-21 10:49:17 +0530

    Time: 11 ms (50.00%) | Memory: 45.8 MB (50.00%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        for(int i = 0 ; i < s.length() ; i++){
+            if(set.contains(s.charAt(i)))return true;
+        }
+        return false;
+    }
+}
