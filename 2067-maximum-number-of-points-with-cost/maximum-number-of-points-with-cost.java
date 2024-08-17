4e63f987b37762b69c77d18e774420a71bc2198f
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-08-17 15:06:39 +0530

    Time: 8 ms (68.82%) | Memory: 92.3 MB (5.30%) - LeetSync

---
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long maxPoints(int[][] points) {
+        int m = points.length;
+        int n = points[0].length;
+
+        long[] dp = new long[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = points[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            long[] left = new long[n];
+            long[] right = new long[n];
+
+            left[0] = dp[0];
+            for (int j = 1; j < n; j++) {
+                left[j] = Math.max(left[j - 1] - 1, dp[j]);
+            }
+
+            right[n - 1] = dp[n - 1];
+            for (int j = n - 2; j >= 0; j--) {
+                right[j] = Math.max(right[j + 1] - 1, dp[j]);
+            }
+
+            for (int j = 0; j < n; j++) {
+                dp[j] = points[i][j] + Math.max(left[j], right[j]);
+            }
+        }
+
+        long ans = Long.MIN_VALUE;
+        for (long val : dp) {
+            ans = Math.max(ans, val);
+        }
+
+        return ans;
+    }
+}
