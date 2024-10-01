25e0d0537c36651b252ac145be2c219ad20fd21f
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-01 12:37:04 +0530

    Time: 3 ms (99.79%) | Memory: 45.8 MB (52.66%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,29 +1,21 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int ans = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(Math.abs(arr[i] - x)< Math.abs(arr[ans]-x)){
-                ans = i;
-            }
-        }
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[ans]);
-        k--;
-        int i = ans-1 ; 
-        int j = ans+1 ;
-        while(k-->0){
-            int prev = i<0?Integer.MIN_VALUE : arr[i];
-            int next = j == arr.length ? Integer.MIN_VALUE : arr[j];
-            if(i==-1 || Math.abs(next - x )< Math.abs(prev - x )){
-                list.add(arr[j]);
-                j++;
+        int i = 0;
+        int j = arr.length-k; 
+        int mid = i+ (j-i)/2;
+        while(i < j ){
+            if(x - arr[mid] > arr[mid+k]-x){
+                i=mid+1;
             }
             else{
-                list.add(arr[i]);
-                i--;
+                j = mid;
             }
+            mid = i+(j-i)/2;
         }
-        Collections.sort(list);
-        return list;
+        List<Integer> ans = new ArrayList<>();
+        int till = i+k;
+        for(; i < till ; i++)
+            ans.add(arr[i]);
+        return ans;
     }
 }
