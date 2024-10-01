621f0a243656b2afec65ea6147b5fdc53603d2c2
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-01 17:31:13 +0530

    Time: 565 ms (9.98%) | Memory: 69.5 MB (64.79%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,42 +1,29 @@
 class NumMatrix {
-    int arr[][];
-    
+    HashMap<Integer , List<Integer>> map;
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        arr = new int[m][n];
-        
-        // Create the cumulative sum matrix
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = matrix[i][j];
-                if (i > 0) arr[i][j] += arr[i - 1][j];
-                if (j > 0) arr[i][j] += arr[i][j - 1];
-                if (i > 0 && j > 0) arr[i][j] -= arr[i - 1][j - 1];
-            }
+        map = new HashMap<>();
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                if(map.containsKey(i) == false){
+                    map.put(i , new ArrayList<>());
+                    map.get(i).add(0);
+                }
+                map.get(i).add(map.get(i).get(map.get(i).size()-1)+matrix[i][j]);
+            }   
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = arr[row2][col2];
-        
-        if (row1 > 0) {
-            ans -= arr[row1 - 1][col2];
+        int sum = 0; 
+        for(int i = row1 ; i <= row2 ; i++){
+            sum+=(map.get(i).get(col2+1) - map.get(i).get(col1));
         }
-        
-        if (col1 > 0) {
-            ans -= arr[row2][col1 - 1];
-        }
-        
-        if (row1 > 0 && col1 > 0) {
-            ans += arr[row1 - 1][col1 - 1];
-        }
-        return ans;
+        return sum;
     }
 }
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1, col1, row2, col2);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
