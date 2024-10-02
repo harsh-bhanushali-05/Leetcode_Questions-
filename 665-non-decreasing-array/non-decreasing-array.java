19cb407aec0a567ae8b2b89229861d47f86d85de
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-02 12:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (55.89%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.java
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        for(int i = 0 ; i < nums.length ; i++){
-            int max = Integer.MIN_VALUE;
-            int count = 0;
-            int index = 0;
-            for(int j : nums ){
-                if(index == i ){
-                    index++;
-                    continue;
+        int count = 0;
+        for(int i = 0 ; i < nums.length-1 ; i++){
+            if(nums[i] > nums[i+1]){
+                count++;
+                if(i!=0 && nums[i-1] > nums[i+1]){
+                    nums[i+1] = nums[i]; 
                 }
-                if(j < max ){
-                    count++;
-                    if(count == 1)break;
-                }
-                max = Math.max(max , j );
-                index++;
+                if(count >1 )return false;
             }
-            if(index == nums.length)return true;
         }
-        return false;
+        return true;
     }
 }
