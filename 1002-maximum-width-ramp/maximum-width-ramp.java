7408ba71b384a65d7d07dbe737769eb8881aaf63
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-10 11:15:59 +0530

    Time: 35 ms (15.42%) | Memory: 55.7 MB (18.41%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,27 +1,17 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Solution {
     public int maxWidthRamp(int[] nums) {
         int n = nums.length;
         Integer[] indices = new Integer[n];
-
-        // Step 1: Create an array of indices and sort it based on the values in nums
         for (int i = 0; i < n; i++) {
             indices[i] = i;
         }
-
         Arrays.sort(indices, Comparator.comparingInt(i -> nums[i]));
-
         int maxWidth = 0;
-        int minIndex = indices[0]; // Initialize the minimum index
-
-        // Step 2: Traverse the sorted indices to find the maximum width ramp
+        int minIndex = indices[0]; 
         for (int i = 1; i < n; i++) {
             maxWidth = Math.max(maxWidth, indices[i] - minIndex);
             minIndex = Math.min(minIndex, indices[i]);
         }
-
         return maxWidth;
     }
 }
