32bc4c1bbea87709df07a438b57bd703d18e734d
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-17 11:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (98.94%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,24 +1,26 @@
 class Solution {
     public int maximumSwap(int num) {
-        char c[] = Integer.toString(num).toCharArray();
+        char[] digits = Integer.toString(num).toCharArray();
+        int[] last = new int[10]; // To store the last occurrence of each digit (0-9)
+        for (int i = 0; i < digits.length; i++) {
+            last[digits[i] - '0'] = i;
+        }
 
-        for(int i = 0 ; i < c.length ; i++){
-            int max = i;
-            for(int j = i+1 ; j < c.length ; j++){
-                if(Integer.parseInt(Character.toString(c[i]))!=Integer.parseInt(Character.toString(c[j]))&& Integer.parseInt(Character.toString(c[j])) >= Integer.parseInt(Character.toString(c[max])))
-                    max = j;
-            }
-            if(max!=i){
-                char temp = c[i];
-                c[i] = c[max];
-                c[max] =temp;
-                break;
+        // Try to find the first place where we can swap to get a larger number
+        for (int i = 0; i < digits.length; i++) {
+            // Check from digit 9 to current digit+1 to find a larger digit to swap
+            for (int d = 9; d > digits[i] - '0'; d--) {
+                if (last[d] > i) {
+                    // Swap digits[i] with digits[last[d]]
+                    char temp = digits[i];
+                    digits[i] = digits[last[d]];
+                    digits[last[d]] = temp;
+                    // Return the integer after the swap
+                    return Integer.parseInt(new String(digits));
+                }
             }
         }
-        String str = "";
-        for(char a : c ){
-            str+=a;
-        }
-        return Integer.parseInt(str);
+        // Return the original number if no swap makes it larger
+        return num;
     }
 }
