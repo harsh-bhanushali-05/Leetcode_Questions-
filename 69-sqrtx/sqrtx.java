b44ffe411b639ea0a1e48578b57a4e332ea6a7c2
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-28 10:59:22 +0530

    Time: 1 ms (85.65%) | Memory: 41.1 MB (13.76%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,6 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        // find the sqrt of the current number 
-        return (int)Math.sqrt(x);
+        int i = 0;
+        int j = x;
+        while (j >= i) {
+            int mid = i + (j - i) / 2;
+            long square = (long) mid * mid; // cast to long to prevent overflow
+
+            if (square == x) return mid;
+            if (square > x) j = mid - 1;
+            else i = mid + 1;
+        }
+        return j;
     }
 }
