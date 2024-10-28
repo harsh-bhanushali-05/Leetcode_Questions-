a0a6dd971fa4611767ec3bb6617e2af367a60177
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-28 14:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (48.21%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int f(int i , int nums[] , int dp[]){
+        if(nums.length <= i ){
+            return 0;
+        }
+        if(dp[i]!=-1)return dp[i];
+        int take = f(i+2 , nums, dp )+nums[i];
+        int not = f(i+1 , nums ,dp);
+        return dp[i]=Math.max(take , not );
+    }
+    public int rob(int[] nums) {
+        int dp[]= new int[nums.length];
+        Arrays.fill(dp , -1 );
+        return f(0 , nums , dp );
+    }
+}
