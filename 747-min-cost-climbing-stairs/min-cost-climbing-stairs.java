35e68401e25e70296791e2e441b25d82b14e83e1
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-28 13:01:10 +0530

    Time: 1 ms (38.75%) | Memory: 43.7 MB (8.24%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int f(int i , int cost[] , int dp[]){
-        if(i >=cost.length)return 0;
+    public int f(int i , int nums[] ,int dp[] ){
+        if(nums.length <= i ){
+            return 0;
+        }
         if(dp[i]!=-1)return dp[i];
-        int one = f(i+1 , cost , dp );
-        int two = f(i+2 , cost, dp );
-        return dp[i] = Math.min(one ,two)+cost[i];
+        int one = f(i + 2 , nums, dp );
+        int two = f(i + 1 , nums ,dp );
+        return dp[i] = Math.min(one + nums[i], two + nums[i]);
     }
     public int minCostClimbingStairs(int[] cost) {
+        // what is the min cost. 
+        // start at 0 or 1 
         int dp[] = new int[cost.length];
-        Arrays.fill(dp,-1);
-        int x = f(0, cost,dp);
-        Arrays.fill(dp,-1);
-        int y = f(1 , cost ,dp);
-        return Math.min( x, y);
+        Arrays.fill(dp , -1 );
+        int zero = f(0 , cost ,dp);
+        Arrays.fill(dp , -1 );
+        int one = f(1 , cost , dp );
+        return Math.min(zero , one );
     }
 }
