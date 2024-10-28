b39234257677a87d7aa4dd673e94b8218700c741
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-28 15:08:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (76.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int f(int i , int nums[] , int dp[]){
-        if(nums.length <= i ){
-            return 0;
-        }
-        if(dp[i]!=-1)return dp[i];
-        int take = f(i+2 , nums, dp )+nums[i];
-        int not = f(i+1 , nums ,dp);
-        return dp[i]=Math.max(take , not );
-    }
     public int rob(int[] nums) {
-        int dp[]= new int[nums.length];
-        Arrays.fill(dp , -1 );
-        return f(0 , nums , dp );
+        int dp[]= new int[nums.length+2];
+        Arrays.fill(dp , 0 );
+        for(int i = nums.length-1 ; i >= 0 ; i--){
+            dp[i] = Math.max(dp[i+2] + nums[i] , dp[i+1]);
+        }
+        return dp[0];
     }
 }
 // this is the memoization solution for our questions 
