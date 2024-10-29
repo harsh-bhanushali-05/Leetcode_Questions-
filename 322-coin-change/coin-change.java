e752668e199284962482f66db2544ff6cfc67022
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-29 19:07:42 +0530

    Time: 33 ms (18.07%) | Memory: 45.5 MB (11.16%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int f(int arr[] , int i , int amt , int dp[][]){
-        if(amt == 0 )return 0;
-        if(i == arr.length)return (int)1e9;
-        if(amt<0) return(int)1e9;
-        if(dp[i][amt]!=-1)return dp[i][amt];
-        int take = f(arr , i , amt-arr[i] ,dp)+1;
-        int not = f(arr ,i+1 , amt ,dp);
-        return dp[i][amt]= Math.min(take , not);
+    public int f(int arr[] , int i , int target , int dp[][]){
+        if(target == 0 )return 0;
+        if(target < 0 || i >= arr.length)return (int)1e9;
+        if(dp[i][target] !=-1)return dp[i][target];
+        int take = f(arr , i , target - arr[i],dp )+1;
+        int not = f(arr , i+1 , target ,dp );
+        return dp[i][target]= Math.min(take , not );
     }
     public int coinChange(int[] coins, int amount) {
-        int dp[][]=new int[coins.length][amount+1];
-        for(int x[]:dp)
-            Arrays.fill(x , -1 );
-        int ans=f(coins , 0 , amount , dp );
-        if(ans>(int)1e8)return -1;
+        int dp[][] = new int[coins.length][amount+1];
+        for(int i[]: dp)
+            Arrays.fill(i , -1 );
+        int ans = f(coins,0 , amount , dp );
+        if(ans >= (int)1e8)return -1;
         return ans;
     }
 }
