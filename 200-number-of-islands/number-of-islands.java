f6d700f24714141d170a643963256a542aabf1a7
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-10-29 14:25:42 +0530

    Time: 4 ms (49.73%) | Memory: 49 MB (87.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,25 +1,25 @@
 class Solution {
-    public void dfs(char grid[][] , int i , int j , int vis[][]){
-        if(i < 0 || j<0 || i>=grid.length || j>=grid[0].length )return;
-        if(vis[i][j]==1)return;
-        if(grid[i][j] == '0')return;
-        vis[i][j] = 1 ;
-        int row[] = {1,0,-1,0};
-        int col[] = {0,1,0,-1};
-        for(int a = 0;a<4;a++){
-            dfs(grid , i+row[a] , col[a]+j , vis);
-        }
+    public void f(int i , int j , char arr[][] , int vis[][]){
+        if(i < 0 || j < 0 || i>= arr.length || j >=arr[0].length || arr[i][j] == '0'|| vis[i][j]==1){
+            return;
+        } 
+        vis[i][j] = 1;
+        f(i +1 , j , arr , vis);
+        f(i , j +1, arr , vis);
+        f(i - 1 , j , arr , vis);
+        f(i , j-1 , arr , vis);
     }
     public int numIslands(char[][] grid) {
-        int vis[][] = new int[grid.length][grid[0].length];
-        for(int i[]:vis)
-            Arrays.fill(i , 0 );
+        int vis[][]= new int[grid.length][grid[0].length];
+        for(int i[] : vis ){
+            Arrays.fill(i , -1);
+        }
         int ans = 0 ;
-        for(int i = 0 ; i< vis.length ; i++){
-            for(int j = 0 ; j<vis[0].length ; j++){
-                if(grid[i][j] == '1' && vis[i][j] ==0 ){
-                    dfs(grid , i , j , vis );
+        for(int i = 0 ; i < grid.length ; i++){
+            for(int j = 0 ; j < grid[0].length ; j++){
+                if(grid[i][j] == '1' && vis[i][j]!=1){
                     ans++;
+                    f(i , j , grid , vis);
                 }
             }
         }
