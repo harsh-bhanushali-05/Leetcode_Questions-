970018460024205a810a4f53ad8bffbad2ac4f3a
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-12-19 17:29:37 +0530

    Time: 1 ms (16.32%) | Memory: 42.9 MB (81.39%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,24 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int max = -1;
-        for(int i = nums.length-1 ; i>=0 ; i--){
-            if(nums[i] < max ){
-                // change this element 
-                int flip = Integer.MAX_VALUE;
-                int index = -1;
+        int max = 0; 
+        for(int i = nums.length -1 ; i >=0 ; i--){
+            max = Math.max(max , nums[i]); 
+            if(max > nums[i]){
+                // we have found the element 
+                // find the smallest greater element 
+                int exchange = -1 ; 
                 for(int j = i+1 ; j < nums.length ; j++){
-                    if(nums[j] > nums[i] && flip > nums[j]){
-                        flip = nums[j];
-                        index = j;
+                    if(nums[j] > nums[i] && (exchange == - 1 || nums[exchange] > nums[j])){
+                        exchange = j ; 
                     }
                 }
-                nums[index] = nums[i];
-                nums[i] = flip;
-                Arrays.sort(nums , i+1 , nums.length);
+                // we found the element index to change with 
+                int temp= nums[i]; 
+                nums[i] = nums[exchange]; 
+                nums[exchange] = temp;
+                Arrays.sort(nums , i+1 , nums.length); 
                 return;
             }
-            max = Math.max(max , nums[i]);
         }
-        Arrays.sort(nums);
+        Arrays.sort(nums); 
+        return; 
     }
 }
