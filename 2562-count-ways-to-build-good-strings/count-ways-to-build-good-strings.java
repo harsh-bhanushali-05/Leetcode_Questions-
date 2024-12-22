6d056b9fdacb64f732eb0060ee13a426a8c47adf
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-12-22 20:16:28 +0530

    Time: 7 ms (92.47%) | Memory: 44.5 MB (43.73%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,7 +1,6 @@
 class Solution {
     public long f(int low , int high , int zero , int one , int curr , long dp[] ){
         int MOD = (int)1e9 + 7;
-
         if(curr > high )return 0; 
         if(dp[curr] !=-1)return dp[curr];
         long ans = 0; 
@@ -13,8 +12,16 @@ class Solution {
     }
     public int countGoodStrings(int low, int high, int zero, int one) {
         int MOD = (int)1e9 + 7;
-        long dp[] = new long[high+1];
-        Arrays.fill(dp , -1 );
-        return (int)(f(low , high , zero , one , 0 , dp)% MOD);
+        long dp[] = new long[high+1 + zero + one +2];
+        // Arrays.fill(dp , -1 );
+        Arrays.fill(dp , 0 );
+        // return (int)(f(low , high , zero , one , 0 , dp)% MOD);
+        for(int i = high ; i >= 0 ; i--){
+            dp[i] = (dp[i+zero] + dp[i+one] )%MOD;
+            if(i <= high && i >=low){
+                dp[i]++;
+            } 
+        }
+        return (int)(dp[0]%MOD);
     }
 }
