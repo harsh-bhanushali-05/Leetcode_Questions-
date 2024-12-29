674d193ae8d2e9eee06073b331328633104eba53
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-12-29 17:14:30 +0530

    Time: 717 ms (13.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,1 @@
+select query_name , round(AVG(rating / position),2) as quality , round(count(Case when rating < 3 then 1 end ) / count(rating)*100,2) as poor_query_percentage from Queries group by query_name;
