6193d89b6ac9b56f6984f607f7b417b1330de318
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2025-01-01 18:31:30 +0530

    Time: 513 ms (71.38%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,8 +1,2 @@
-SELECT 
-    activity_date AS day, 
-    COUNT(DISTINCT user_id) AS active_users
-FROM 
-    Activity
-WHERE 
-    DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date)>=0
-GROUP BY 1
+-- This is the 6th question of the day 
+select activity_date as day , count(distinct user_id) as active_users from Activity where    DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date)>=0 group by activity_date ;
