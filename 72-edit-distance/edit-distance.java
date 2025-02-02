85e997b542093d86456fa8881e6a60a2e0d71e74
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2025-02-02 10:32:34 +0530

    Time: 3 ms (95.10%) | Memory: 45.4 MB (19.21%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,26 +1,34 @@
 class Solution {
-    public int f(int i , int j , String a , String b , int dp[][] ){
-        if(i>=a.length()){
-            // word 1 is over return all the characters of j left 
-            return b.length()-j;
-        }
-        if(j>=b.length()){
-            // all the characters of b are done we need to delete the character of a now 
+    public int f(int i, int j, String a, String b, int dp[][]) {
+        if (j == b.length()) {
+            if (i == a.length()) {
+                return 0;
+            }
             return a.length() - i;
         }
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)){
-            return dp[i][j] = f(i+1 , j+1 , a,b , dp);
+        if (i == a.length()) {
+            return b.length() - j;
         }
-        else{
-            return dp[i][j] = 1 + Math.min(f(i , j +1 , a, b , dp) , Math.min(f(i+1 , j ,a , b ,dp) , f(i+1 , j+1 ,a , b ,dp)));
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (a.charAt(i) == b.charAt(j)) {
+            return dp[i][j] = f(i + 1, j + 1, a, b, dp);
         }
+        int insert = f(i, j + 1, a, b, dp) + 1;
+        int delete = f(i + 1, j, a, b, dp) + 1;
+        int replace = f(i + 1, j + 1, a, b, dp) + 1;
+        return dp[i][j] = Math.min(insert, Math.min(delete, replace));
+        // insert -> i , j+1
+        // delete -> i+1 , j
+        // replace -> i+1 , j+1
+        // return the min of the two
     }
+
     public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length()][word2.length()];
-        for(int i[] : dp ){
-            Arrays.fill(i , -1 );
+        for (int i[] : dp) {
+            Arrays.fill(i, -1);
         }
-        return f(0 , 0 , word1 , word2 , dp );
+        return f(0, 0, word1, word2, dp);
     }
 }
