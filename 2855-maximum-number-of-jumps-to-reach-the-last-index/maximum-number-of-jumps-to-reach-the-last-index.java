b65c71bf46eea59f8f25012f5bf7977ddea7827f
Harsh Bhanushali <109029538+harsh-bhanushali-05@users.noreply.github.com>
2026-05-10 18:45:09 +0530

    Time: 21 ms (23.21%) | Memory: 46.6 MB (98.21%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int f(int i , int nums[] , int target , int dp[]){
-        if(i == nums.length-1){
+    public int f(int i , int nums[] , int k , int dp[] ){
+        if(i == nums.length-1 ){
             return 0;
         }
-        if(dp[i]!=-1)return dp[i];
-        int ans = Integer.MIN_VALUE;
+        if(dp[i]!= -1 ){
+            return dp[i];
+        }
+        int ans = Integer.MIN_VALUE; 
         for(int j = i+1 ; j < nums.length ; j++){
-            if(Math.abs(nums[j] - nums[i]) <= target){
-                ans = Math.max(ans , f(j , nums , target, dp)+1);
+            if(Math.abs(nums[i]-nums[j]) <= k ){
+                int curr = f(j , nums , k ,dp ) ;
+                if(curr!= Integer.MIN_VALUE ){
+                    ans = Math.max(curr+1 , ans);
+                }
             }
         }
         return dp[i] = ans;
     }
     public int maximumJumps(int[] nums, int target) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp , -1 );
-        int ans = f(0, nums , target , dp );
-        return ans <0? -1: ans ;   
+        int dp[] = new int[nums.length]; 
+        Arrays.fill(dp , - 1);
+        int ans = f(0 , nums , target , dp );
+        return ans > Integer.MIN_VALUE ? ans : -1 ;
     }
 }
