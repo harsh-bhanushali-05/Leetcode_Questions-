eb7889615d90f8d227aca805a7c4cde7bb7e6afd
Harsh Bhanushali <109029538+harsh-bhanushali-05@users.noreply.github.com>
2026-05-28 11:26:17 +0530

    Time: 6 ms (61.36%) | Memory: 42.8 MB (68.11%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+        // return the number of panlinromic string 
+        int ans = 0 ;
+        for(int i = 0 ; i < s.length() ; i++ ){
+            int left = i; 
+            int right = i; 
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                ans++;
+                left--;
+                right++;
+            }
+            left = i;
+            right = i+1;
+            while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+}
