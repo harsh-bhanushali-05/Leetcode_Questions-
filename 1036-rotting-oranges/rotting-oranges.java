d89f310fa42e506ca29ab89467cf2c5923c6690b
Harsh Bhanushali <109029538+harsh-bhanushali-05@users.noreply.github.com>
2026-06-18 23:37:50 +0530

    Time: 12 ms (3.40%) | Memory: 44.4 MB (24.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,47 +1,45 @@
 class Solution {
-    class pair{
-        int x;
-        int y;
-        int time; 
-        pair(int x , int y , int time ){
-            this.x=x;
-            this.y=y;
-            this.time= time;
+    class Node{ 
+        int i , j , time; 
+        Node(int i , int j , int time ){
+            this.i= i ; 
+            this.j = j; 
+            this.time = time; 
         }
     }
     public int orangesRotting(int[][] grid) {
-        int ans = 0 ;
-        PriorityQueue<pair> pq = new PriorityQueue<>((a,b)->a.time - b.time);
-        for(int i = 0 ; i < grid.length ; i++){
+        Queue<Node> q = new ArrayDeque<>(); 
+        for(int i =0;i<grid.length;i++){
             for(int j = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == 2 )
-                    pq.offer(new pair(i , j , 0 )); // adding the rotten orange to the queue 
-            }
-        }
-        while(pq.isEmpty()==false){
-            pair curr = pq.poll();
-            int x = curr.x;
-            int y = curr.y;
-            int time = curr.time; 
-            ans = Math.max(ans , time);
-            int row[] = {1,0,-1,0};
-            int col[] = {0 ,1 , 0 , -1 };
-            for(int i = 0 ; i < 4 ; i++){
-                int newx = row[i] + x;
-                int newy = col[i] + y;
-                if(newx < 0 || newx >= grid.length || newy < 0 || newy >=grid[0].length )continue;
-                if(grid[newx][newy] == 1 ){
-                    // this is a fresh orange 
-                    pq.offer(new pair(newx , newy , time+1));
-                    grid[newx][newy] = 2;
+                if(grid[i][j] == 2 ){
+                    q.offer(new Node(i , j , 0 ));
                 }
             }
         }
-        for(int i[] : grid){
-            for(int j : i ){
-                if(j == 1 )return -1;
+        int row[] = {1,0,-1,0};
+        int col[] = {0 , -1 , 0 , 1 };
+        int time = 0 ; 
+        while(q.isEmpty()==false){
+            Node curr = q.poll(); 
+            for(int a = 0 ; a < 4 ; a++){
+                int rowx= curr.i + row[a];
+                int colx = curr.j + col[a]; 
+                if(rowx < 0 || colx< 0 || rowx>= grid.length || colx >= grid[0].length ){
+                    continue;
+                }
+                if(grid[rowx][colx] == 1 ){
+                    q.offer(new Node(rowx , colx , curr.time+1));
+                    time = Math.max(time , curr.time+1);
+                    grid[rowx][colx] = 2;
+                }
             }
         }
-        return ans;
+        boolean list =Arrays.stream(grid)
+                       .flatMapToInt(Arrays::stream)
+                       .anyMatch(val -> val == 1);
+        if(list){
+            return -1; 
+        }
+        return time;
     }
-}                                             
+}
