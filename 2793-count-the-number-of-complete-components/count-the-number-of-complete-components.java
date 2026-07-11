ce1180173a8f209178b71037cca92f332e86c613
Harsh Bhanushali <109029538+harsh-bhanushali-05@users.noreply.github.com>
2026-07-11 17:38:43 +0530

    Time: 50 ms (5.36%) | Memory: 47.7 MB (39.49%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,47 @@
+class Solution {
+    public HashMap<Integer , List<Integer>> nodelist(int n , int arr[][] ){
+        HashMap<Integer , List<Integer>> map = new HashMap<>(); 
+        for(int i = 0; i < n ; i++){
+            map.put(i , new ArrayList<>());
+            map.get(i).add(i);
+        }
+        for(int i[] : arr){
+            map.get(i[0]).add(i[1]);
+            map.get(i[1]).add(i[0]);
+        }
+        return map; 
+    }
+    public boolean isSame(List<Integer> a , List<Integer> b ){
+        // Quick size check first for performance
+        if (a.size() != b.size()) {
+            return true;
+        }
+        // Use HashSets to compare elements without worrying about order
+        HashSet<Integer> setA = new HashSet<>(a);
+        HashSet<Integer> setB = new HashSet<>(b);
+        return !setA.equals(setB); 
+    }
+    public boolean isConnected(int i , HashMap<Integer , List<Integer>> map){
+        List<Integer> curr = map.get(i);
+        for(int nbr : curr){
+            if(isSame(map.get(nbr) , curr)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+        HashMap<Integer , List<Integer>> map = nodelist(n , edges );
+        int ans = 0; 
+        HashSet<Integer> set = new HashSet<>(); 
+        for(int i =0 ; i < n ; i++){
+            if(set.contains(i) == false && isConnected(i , map )){
+                ans++;
+            }
+            for(int val : map.get(i)){
+                set.add(val);
+            }
+        }
+        return ans; 
+    }
+}
